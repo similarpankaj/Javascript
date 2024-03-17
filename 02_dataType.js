4255234acde41de500a77a6f6bdcaf4cd198b719
Pankaj <pankaj.upadhyay269@gmail.com>
2024-03-17 20:17:21 +0530

    data type

---
+++ 02_dataType.js
@@ -0,0 +1,16 @@
+"use strict";
+let age = 5
+
+// number = 2 to power 53
+//bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefine => 
+// symbol => unique
+
+//object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
