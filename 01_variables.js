805d5b3e01944755bc53237942eda245a456aac6
Pankaj <pankaj.upadhyay269@gmail.com>
2024-03-17 20:17:40 +0530

    variables

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 34325235
+let accountEmail = 'pankaj.upadhyay@gmail.com'
+var accountPassword = '12345'
+accountCity = 'agra'
+let accountState;
+
+// accountId = 2
+
+// console.log(accountId)
+accountEmail = 'pu@gmail.com'
+accountCity = 'pune'
+
+console.table([accountEmail, accountId, accountPassword,accountCity,accountState ])
